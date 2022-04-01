e90313394f4d1adacad536b807f432de0eea5db8
Syakir Rahman <syakirurahman@gmail.com>
2022-04-01 20:22:37 +0700

    Added app layout

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../pages/counter/counterSlice';
+import appLayout from '../app-layout.slice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    appLayout
   },
 });
 
